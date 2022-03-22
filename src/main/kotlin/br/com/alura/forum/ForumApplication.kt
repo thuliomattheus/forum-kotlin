464a31ea33a33db231cebe42e103402c45dd7430
Thúlio Sousa <thuliomattheus@hotmail.com>
2022-03-22 17:46:43 -0300

    introducing caching

--- src/main/kotlin/br/com/alura/forum/ForumApplication.kt
+++ src/main/kotlin/br/com/alura/forum/ForumApplication.kt
@@ -4,8 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
 
-@SpringBootApplication
-@EnableCaching
+@SpringBootApplication @EnableCaching
 class ForumApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/br/com/alura/forum/controller/CourseController.kt
+++ src/main/kotlin/br/com/alura/forum/controller/CourseController.kt
@@ -3,13 +3,14 @@ package br.com.alura.forum.controller
 import br.com.alura.forum.dto.NewCourseForm
 import br.com.alura.forum.entity.Course
 import br.com.alura.forum.service.CourseService
+import org.springframework.cache.annotation.CacheEvict
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.util.*
-import javax.transaction.Transactional
 import javax.validation.Valid
 
 @RestController
@@ -17,12 +18,14 @@ import javax.validation.Valid
 class CourseController(private val courseService: CourseService) {
 
     @GetMapping
+    @Cacheable("courses")
     fun findAll(pageable: Pageable): ResponseEntity<Page<Course>> = ResponseEntity.ok(courseService.findAll(pageable))
 
     @GetMapping("/{id}")
     fun findById(@PathVariable id: Long): ResponseEntity<Optional<Course>> = ResponseEntity.ok(courseService.findById(id))
 
     @PostMapping
+    @CacheEvict("courses", allEntries = true)
     fun save(@RequestBody @Valid newCourseDto: NewCourseForm): ResponseEntity<Course> = ResponseEntity(
         courseService.save(newCourseDto),
         HttpStatus.CREATED
